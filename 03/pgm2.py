8e29f7ab8b06006306a3a0dcff763f26309e7731
Philippe Chataignon <philippe.chataignon@gmail.com>
2021-12-03 23:36:03 +0100

    Remove unused lines

--- 03/pgm2.py
+++ 03/pgm2.py
@@ -3,10 +3,7 @@ def main():
     with open("input.txt") as f:
         r = [list(l[:-1]) for l in f]
     l = len(r[0])   #nb cols
-    n = len(r)      #nb rows
-    ones = [0] * l
-    for i in range(l):
-        ones[i] = sum([1 if x[i] == '1' else 0 for x in r])
+
     r1 = r.copy()
     i = 0
     while i < l and len(r1) > 1:
@@ -15,14 +12,13 @@ def main():
         r1 = [x for x in r1 if x[i] == bit]
         i += 1
     a = eval("0b" + "".join(r1[0]))
+
     r2 = r.copy()
     i = 0
     while i < l and len(r2) > 1:
         ones = sum([1 if x[i] == '1' else 0 for x in r2])
         bit = "0" if ones >= len(r2) / 2 else "1"
-        print(ones, bit)
         r2 = [x for x in r2 if x[i] == bit]
-        print(r2)
         i += 1
     b = eval("0b" + "".join(r2[0]))
     print(a * b)
