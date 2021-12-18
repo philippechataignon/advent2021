a127f70f73d0b49e1eb2bfd465b69e4a9187079c
Philippe Chataignon <philippe.chataignon@gmail.com>
2021-12-18 17:48:49 +0100

    CLean pgm2

--- 14/pgm2.py
+++ 14/pgm2.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-def apply(cmd, poly):
-    ret = poly[0]
-    for s in range(1, len(poly)):
-        state = poly[s - 1]
-        ret += cmd[state][poly[s]]
-    return ret
 
 def main():
     f = open("input.txt")
     start = f.readline()[:-1]
     duo_start = [start[i:i+2] for i in range(len(start) - 1)]
-    print(start, duo_start)
     f.readline()
     cmd_duo = {}
     cmd_let = {}
@@ -21,7 +14,6 @@ def main():
         p, m = l[:-1].split(" -> ")
         cmd_duo[p] = [p[0] + m, m + p[1]]
         cmd_let[p] = m
-    print(cmd_duo, cmd_let)
     let_count = {}
     duo_count = {}
     for k in cmd_duo:
@@ -34,7 +26,7 @@ def main():
         let_count[c] += 1
     for i in range(40):
         eff = duo_count.copy()
-        for x in duo_count:
+        for x in eff:
             n = eff[x]
             duo_count[x] -= n
             t = cmd_duo[x]
@@ -42,7 +34,6 @@ def main():
             duo_count[t[1]] += n
             l = cmd_let[x]
             let_count[l] += n
-    print(let_count)
     print(max(let_count.values()) - min(let_count.values()))
 
 if __name__ == '__main__':
